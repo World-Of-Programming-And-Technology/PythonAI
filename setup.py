28a0ad16141e98f3f199ed1019eb1b4c0d6daad7
Divyanshu Sinha <divyanshu.sinha136@gmail.com>
2025-05-07 15:03:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='PythonAI',
-    version='0.1.0',
-    description='Make AI Brain with pyai',
+    name='PythonAIBrain',  
+    version='0.1.0',  
+    packages=find_packages(), 
+    install_requires=[
+        'nltk',
+        'pyjokes',
+        'yfinance',
+        'numpy',
+        'scikit-learn',
+        'torch',
+    ],
+    author='Divyanshu Sinha',
+    description='Create AI with PythonAIBrain on using its Brain class.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author='Divyanshu Sinha',
-    author_email='divyanshu.sinha136@gmail.com',
-    url='https://www.github.com/World-Of-Programming-And-Technology/PythonAI',
-    packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU Lesser General Public License v2.1',
+        'License :: OSI Approved :: MIT License',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7', 
 )
